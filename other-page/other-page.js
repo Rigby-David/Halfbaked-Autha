f18f1318e2d0d956ba80b8ffcfd888ca0f05fb76
David <Rigby335@hotmail.com>
2022-05-16 14:17:53 -0700

    checkAuth and logoutButton functionality

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,4 +1,11 @@
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
-const logout = document.getElementById('logout');
+import { checkAuth, logout } from '../fetch-utils.js';
 
+const logoutButton = document.getElementById('logout');
+
+checkAuth();
+
+logoutButton.addEventListener('click', async () => {
+    await logout();
+});
